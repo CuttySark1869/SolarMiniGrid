8159c8c3760bd14f858d22e53494c6286f3b6d65
Yunjie Gu <yunjie.gu1@gmail.com>
2023-02-09 17:47:53 +0000

    Parameter

--- PythonScript/DBExport.py
+++ PythonScript/DBExport.py
@@ -1,11 +1,11 @@
 import sqlite3
 import pandas as pd
-conn = sqlite3.connect('StuderOpearion.db')
+conn = sqlite3.connect('DataLog.db')
 
-data_log = pd.read_sql_query('SELECT * FROM StuderOperation', conn)
+data_log = pd.read_sql_query('SELECT * FROM DataLog', conn)
 conn.close()
 
 columns_names = ['DateTime', 'Battery_SOC', 'Battery_Current', 'Battery_Voltage', 'Battery_Power', 'AC_In_Current', 'AC_In_Voltage', 'AC_In_Power']
 data_log.columns = columns_names
 
-data_log.to_csv('StuderOpearion.csv', index=None)
+data_log.to_csv('DataLog.csv', index=None)
