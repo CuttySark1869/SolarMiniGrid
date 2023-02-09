0520942816fe93a3bd0c411bf22141806c53e07d
Yunjie Gu <yunjie.gu1@gmail.com>
2023-02-09 13:15:04 +0000

    Create testsched.py

---
+++ Python Script/testsched.py
@@ -0,0 +1,19 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def my_job_function():
+  print('function 1')
+
+def another_job_function():
+  print('function 2')
+
+
+if __name__ == '__main__':
+  scheduler = BackgroundScheduler()
+
+  # Add some jobs to the scheduler
+  scheduler.add_job(my_job_function, 'interval', seconds=1)
+  scheduler.add_job(another_job_function, 'interval', seconds=2)
+
+  # Start the scheduler
+  scheduler.start()
