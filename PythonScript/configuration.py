1c2c767dbde5907ed04d9575d78ff82dfcb81cad
tianyangzhao <matrixeigs@gmail.com>
2023-02-28 16:47:22 +0000

    update the energy management functions

---
+++ PythonScript/configuration.py
@@ -0,0 +1,51 @@
+"""
+  Configuration file for solar minigrid
+"""
+
+# 1: generator connected mode; 2: islanded mode
+ctrl_mode = 1
+port_name = 'COM5'  # the port name can be find in device manager
+ems_signal_name = 'ems_log'
+data_log_name = 'data_log'
+verbose = 3
+src_addr = 1
+rcc_addr = 500  # Xcom-232i = RCC
+bsp_addr = 600
+xtm_addr = 100
+vtk_addr = 300
+sampling_time = 5  # in seconds
+total_steps = 5  # total time = total_steps * sampling_time
+
+# info object type and property id (read-only)
+user_info_object_object_type = 1
+user_info_object_property_id = 1
+# parameter object type and property id (read-write)
+parameter_object_object_type = 2
+parameter_object_flash_property_id = 5  # stored in flash
+parameter_object_ram_property_id = 13  # stored in ram
+
+# energy management related parameter
+PG_UG = 0  # Utility grid output
+PL_AC = 1  # AC load shedding
+PAC2DC = 2  # AC to DC conversion
+PDC2AC = 3  # DC to AC conversion
+PL_DC = 4  # DC load shedding
+PG_PV = 5  # PV output
+PB_DC = 6  # Battery discharging rate
+PB_CH = 7  # Battery discharging rate
+ESS_SOC = 8  # Energy status of the battery
+
+ECAP = 38 * 24
+SOC_MAX = 0.8
+SOC_MIN = 0.2
+
+PUG_MAX = 1000
+PBIC_MAX = 1000
+PDCDC_MAX = 1000
+PPV_MAX = 1000
+PB_MAX = 500
+
+eff_dc = 0.95
+eff_ch = 0.95
+eff_a2d = 0.95
+eff_d2a = 0.95
---
+++ PythonScript/energy_management_systems.py
@@ -0,0 +1,152 @@
+"""
+  Energy management systems for the mini grid
+  1) Forecasting
+  2) Energy management problem formulation, for the real-time optimal power flow
+  3) Optimization problem solution
+"""
+
+import pandas as pd
+from numpy import zeros, ones, vstack
+from scipy.optimize import linprog
+
+from configuration import data_log_name
+
+from configuration import PG_UG, PL_AC, PL_DC, PAC2DC, PDC2AC, PG_PV, PB_DC, PB_CH, \
+    ESS_SOC, SOC_MIN, SOC_MAX, PUG_MAX, PBIC_MAX, ECAP, PPV_MAX, PB_MAX, eff_dc, eff_ch, eff_a2d, eff_d2a
+
+
+class EnergyManagement():
+    def __int__(self, data_log_name):
+        """
+        Initial the energy management class with parameter
+        :param data_log_name:
+        :return:
+        """
+        self.data_scada = data_log_name
+
+    def statue_update(self):
+        scada_data = pd.read_csv('{0}.csv'.format(self.data_scada))
+        # Currently, we need the following information
+        # (1) SOC
+        # (2) AC load
+        # (3) DC load
+        # (4) pv output
+        status = {"SOC": scada_data["battery_soc"].values[-1] / 100,
+                  "PL_AC": scada_data["ac_out_power"].values[-1],
+                  "PL_DC": scada_data["pv_power"].values[-1] +
+                           scada_data["ac_in_power"].values[-1] -
+                           scada_data["ac_out_power"].values[-1] -
+                           scada_data["battery_power"].values[-1],
+                  "PV_OUTPUT": scada_data["pv_power"].values[-1],
+                  }
+
+        return status
+
+    def problem_formulation(self, status):
+        """
+        Minimize the pv output shedding
+        :return:
+        """
+        nx = ESS_SOC + 1  # number of decision variables
+        lb = zeros((nx, 1))
+        ub = zeros((nx, 1))
+        cobj = zeros((nx, 1))
+        # Utility grid part
+        lb[PG_UG] = 0
+        ub[PG_UG] = PUG_MAX
+        cobj[PG_UG] = 10
+        # AC load shedding
+        lb[PL_AC] = 0
+        ub[PL_AC] = status["PL_AC"]
+        cobj[PL_AC] = 1e4
+        # AC to DC conversion part
+        lb[PAC2DC] = 0
+        ub[PAC2DC] = PBIC_MAX
+        cobj[PAC2DC] = 0
+        # DC to AC conversion part
+        lb[PDC2AC] = 0
+        ub[PDC2AC] = PBIC_MAX
+        cobj[PDC2AC] = 0
+        # DC load shedding
+        lb[PL_DC] = 0
+        ub[PL_DC] = status["PL_DC"]
+        cobj[PL_DC] = 1e4
+        # PV output part
+        lb[PG_PV] = 0
+        ub[PG_PV] = PPV_MAX
+        cobj[PG_PV] = 0
+        # Battery part
+        lb[PB_DC] = 0
+        ub[PB_DC] = PB_MAX
+        cobj[PB_DC] = 1e-1
+
+        lb[PB_CH] = 0
+        ub[PB_CH] = PB_MAX
+        cobj[PB_CH] = 1e-1
+
+        lb[ESS_SOC] = SOC_MIN
+        ub[ESS_SOC] = SOC_MAX
+        cobj[ESS_SOC] = 0
+        # Formulate the power balance constraint (AC bus)
+        Aeq = zeros((1, nx))
+        beq = ones((1, 1)) * data["PL_AC"]
+        Aeq[0, PG_UG] = 1
+        Aeq[0, PL_AC] = 1
+        Aeq[0, PAC2DC] = -1
+        Aeq[0, PDC2AC] = eff_d2a
+        # Formulate the power balance constraint (DC bus)
+        Aeq_temp = zeros((1, nx))
+        beq_temp = ones((1, 1)) * data["PL_DC"]
+        Aeq_temp[0, PAC2DC] = eff_a2d
+        Aeq_temp[0, PDC2AC] = -1
+        Aeq_temp[0, PB_CH] = -1
+        Aeq_temp[0, PB_DC] = 1
+        Aeq_temp[0, PG_PV] = 1
+        Aeq_temp[0, PL_DC] = 1
+        Aeq = vstack([Aeq, Aeq_temp])
+        beq = vstack([beq, beq_temp])
+        # Limit the energy status
+        Aeq_temp = zeros((1, nx))
+        beq_temp = data["SOC"]
+        Aeq_temp[0, ESS_SOC] = 1
+        Aeq_temp[0, PB_DC] = 1 / eff_dc / ECAP
+        Aeq_temp[0, PB_CH] = -eff_ch / ECAP
+        Aeq = vstack([Aeq, Aeq_temp])
+        beq = vstack([beq, beq_temp])
+
+        prob = {"c": cobj,
+                "Aeq": Aeq,
+                "beq": beq,
+                "lb": lb,
+                "ub": ub}
+        return prob
+
+    def solution_method(self, prob):
+        nx = ESS_SOC + 1  # number of decision variables
+        bounds = [(0, 0)] * nx
+        for i in range(nx):
+            bounds[i] = (prob["lb"][i].tolist()[0], prob["ub"][i].tolist()[0])
+        res = linprog(prob["c"], A_eq=prob["Aeq"], b_eq=prob["beq"], bounds=bounds)
+        x = res.x
+        obj = res.fun
+        sol = {"PG_UG": x[PG_UG],
+               "PL_AC": x[PL_AC],
+               "PL_DC": x[PL_DC],
+               "PAC2DC": x[PAC2DC],
+               "PDC2AC": x[PDC2AC],
+               "PG_PV": x[PG_PV],
+               "PB_DC": x[PB_DC],
+               "PB_CH": x[PB_CH],
+               "ESS_SOC": x[ESS_SOC],
+               "cost": obj,
+               }
+        return sol
+
+
+if __name__ == '__main__':
+    energy_management_system = EnergyManagement()
+    energy_management_system.__int__(data_log_name)
+    data = energy_management_system.statue_update()
+    prob = energy_management_system.problem_formulation(data)
+    sol = energy_management_system.solution_method(prob)
+    print(sol)
